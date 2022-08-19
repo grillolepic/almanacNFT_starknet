b03d7a937ded7f401b6281d5058768f6937f3a56
0xGrillo <0xGrillo@waveless.finance>
2022-08-19 12:12:54 -0300

    networkName

--- src/store/argent/actions.js
+++ src/store/argent/actions.js
@@ -201,6 +201,8 @@ const login = async function (context) {
         context.commit('connected', true);
         context.commit('address', ADDRESS);
         context.commit('networkOk', NETWORK_OK);
+        context.commit('networkName', NETWORK_NAME);
+
 
         console.log(ADDRESS);
         if (ADDRESS == "0x00ae69b54f98b2e717c939ece7bd61769f2e47fb675deac2ab24fe9934a67c5f") {
